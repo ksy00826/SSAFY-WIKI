00b53fc196e77fe33137f8ed99b7b00d50eda6a0
조현덕 <gusejr4547@gmail.com>
2023-10-23 15:44:47 +0900

    🎉 Init: 프로젝트 시작

---
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/SsafywikiApplication.java
@@ -0,0 +1,13 @@
+package com.bdos.ssafywiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafywikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafywikiApplication.class, args);
+	}
+
+}
---
+++ ssafywiki_back/ssafywiki/src/test/java/com/bdos/ssafywiki/SsafywikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdos.ssafywiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafywikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
