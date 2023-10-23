d35671962f9a1614e7a13e01fdc3f1d9e3480c6b
young eun <duddms0190@naver.com>
2023-10-23 17:37:44 +0900

    ✨ Feat: 프론트 라우팅 설정, 기본화면

--- ssafywiki_front/src/App.js
+++ ssafywiki_front/src/App.js
@@ -1,16 +1,32 @@
 import "./App.css";
 
 import React from "react";
+import { Layout } from "antd";
+
 import AppRouter from "./AppRouter";
 import Navbar from "./components/Common/Navbar";
-import Footer from "./components/Common/Footer";
+import MyFooter from "./components/Common/Footer";
+
+const { Header, Footer, Sider, Content } = Layout;
+
 function App() {
   return (
     <div className="App">
-      <Navbar />
-
-      <AppRouter />
-      <Footer />
+      <Layout
+        style={{
+          minHeight: "100vh",
+        }}
+      >
+        <Header>
+          <Navbar />
+        </Header>
+        <Layout>
+          <AppRouter />
+        </Layout>
+        <Footer>
+          <MyFooter />
+        </Footer>
+      </Layout>
     </div>
   );
 }
