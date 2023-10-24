31d037a4e45988c2d1f5949ded55b883cf238dc9
김승연 <love00826@naver.com>
2023-10-24 14:48:37 +0900

    ✨ Feat: docs 관련 entity 생성

---
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/redirect_docs/entity/RedirectDocs.java
@@ -0,0 +1,37 @@
+package com.bdos.ssafywiki.redirect_docs.entity;
+
+import com.bdos.ssafywiki.document.entity.Document;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "redirect_docs")
+public class RedirectDocs {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "redir_docs_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "docs_id")
+    @Column(name = "origin_docs_id")
+    private Document originalDocs;
+
+    @Column(name = "redir_title")
+    private String title;
+
+    @CreationTimestamp
+    @Column(name = "redir_created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "redir_modified_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime modifiedAt;
+
+}
