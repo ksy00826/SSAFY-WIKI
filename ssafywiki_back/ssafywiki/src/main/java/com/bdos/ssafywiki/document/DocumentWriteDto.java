12721494efaceb711ac7f48b92394ad61d02d886
김승연 <love00826@naver.com>
2023-10-24 12:33:36 +0900

    ✨ Feat: document 구조 생성

---
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/document/DocumentWriteDto.java
@@ -0,0 +1,9 @@
+package com.bdos.ssafywiki.document;
+
+import lombok.Getter;
+
+@Getter
+public class DocumentWriteDto {
+    private String title;
+    private String content;
+}
---
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/document/service/DocumentService.java
@@ -0,0 +1,7 @@
+package com.bdos.ssafywiki.document.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentService {
+}
