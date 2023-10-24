8f28b9629fbad92b1fdd3b2ddd6d17edb99364f4
조현덕 <gusejr4547@gmail.com>
2023-10-24 14:41:19 +0900

    ✨ Feat: revision 수정

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/entity/Revision.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/entity/Revision.java
@@ -1,18 +1,18 @@
 package com.bdos.ssafywiki.revision.entity;
 
+import com.bdos.ssafywiki.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Builder
-@Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @EntityListeners(AuditingEntityListener.class)
@@ -53,4 +53,22 @@ public class Revision {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "rev_content_id")
     private Content content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "rev_user_id")
+    private User user;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "rev_docs_id")
+//    private Document document;
+
+    @Builder
+    public Revision(Long diffAmount, Long number, Revision parent, Revision origin, Comment comment, Content content) {
+        this.diffAmount = diffAmount;
+        this.number = number;
+        this.parent = parent;
+        this.origin = origin;
+        this.comment = comment;
+        this.content = content;
+    }
 }
