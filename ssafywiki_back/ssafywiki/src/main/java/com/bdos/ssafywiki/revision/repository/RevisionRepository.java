388f4f28c206c2768f3caed2db9fa27b83eb8536
조현덕 <gusejr4547@gmail.com>
2023-10-24 18:01:11 +0900

    ✨ Feat: revision repo 수정

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/repository/RevisionRepository.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/repository/RevisionRepository.java
@@ -1,8 +1,13 @@
 package com.bdos.ssafywiki.revision.repository;
 
 import com.bdos.ssafywiki.revision.entity.Revision;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RevisionRepository extends JpaRepository<Revision, Long> {
 
+    @Query(value = "SELECT r FROM Revision r JOIN FETCH Comment WHERE r.document.id = : docsId")
+    Page<Revision> findAllByDocumentJoinComment(Long docsId, Pageable pageable);
 }
