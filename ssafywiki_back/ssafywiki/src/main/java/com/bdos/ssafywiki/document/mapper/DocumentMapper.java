1abebd63beba1fa13d43bce13321b0106f379fcf
김승연 <love00826@naver.com>
2023-10-25 15:39:17 +0900

    ✨ Feat: 문서 조회 API 생성

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/document/mapper/DocumentMapper.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/document/mapper/DocumentMapper.java
@@ -3,6 +3,7 @@ package com.bdos.ssafywiki.document.mapper;
 import com.bdos.ssafywiki.document.dto.DocumentDto;
 import com.bdos.ssafywiki.document.entity.Document;
 import com.bdos.ssafywiki.revision.entity.Content;
+import com.bdos.ssafywiki.revision.entity.Revision;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,8 +12,4 @@ public interface DocumentMapper {
 
     Document postToDocument(DocumentDto.Post post);
 
-    @Mapping(source = "document.id", target = "docsId")
-    @Mapping(source = "document.user.id", target = "userId")
-    @Mapping(source = "content.text", target = "content")
-    DocumentDto.Response toResponse(Document document, Content content);
 }
