06d6bfbc4cb59117710edb898629fb9e27361f16
조현덕 <gusejr4547@gmail.com>
2023-10-26 16:31:47 +0900

    ✨ Feat: diff API

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/service/RevisionService.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/service/RevisionService.java
@@ -68,11 +68,73 @@ public class RevisionService {
     }
 
     public LinkedList<Diff> diff(long revId, long oldRevId) {
-//        Revision rev = revisionRepository.findById(revId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVISION_NOT_FOUND));
-//        Revision oldRev = revisionRepository.findById(oldRevId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVISION_NOT_FOUND));
-        String newText = """
+//        String newText = """
+//                ----
+//                {{{#!wiki style="text-align:center"
+//                '''{{{+2 여러분이 가꾸어 나가는 {{{#00a495,#E69720 지식의 나무}}}}}}'''
+//                [[나무위키|{{{#00a495,#E69720 나무위키}}}]]에 오신 것을 환영합니다!
+//                [[:파일:nogray.png|{{{#ffffff,#E69720 다크는 회색이 아니라 검정입니다!}}}]]
+//                나무위키는 누구나 기여할 수 있는 위키입니다.
+//                검증되지 않았거나 편향된 내용이 있을 수 있습니다.
+//                }}}
+//                ----
+//                [include(틀:이용안내)]
+//                ----
+//                [include(틀:나무위키 프로젝트)]
+//                ----
+//                {{{#!wiki style="margin-bottom:-10px"
+//                {{{#!wiki style="margin-bottom:-10px"
+//                [include(위키운영:접근 틀)]
+//                }}}
+//                [include(틀:나무위키)]}}}
+//                ----
+//                [include(틀:운영알림)]
+//                ----
+//                [include(틀:운영토론)][[분류:나무위키]]
+//                ## 운영 토론이나 운영 알림은 해당 틀을 편집해 주시기 바랍니다.
+//                """;
+//        String oldText = """
+//                ----
+//                [include(틀:대문 기념일)]
+//                ----
+//                [include(틀:이용안내)]
+//                ----
+//                [include(틀:나무위키 프로젝트)]
+//                ----
+//                {{{#!wiki style="margin-bottom:-10px"
+//                {{{#!wiki style="margin-bottom:-10px"
+//                [include(위키운영:접근 틀)]
+//                }}}
+//                [include(틀:나무위키)]}}}
+//                ----
+//                [include(틀:운영알림)]
+//                ----
+//                [include(틀:운영토론)][[분류:나무위키]]
+//                ## 운영 토론이나 운영 알림은 해당 틀을 편집해 주시기 바랍니다.
+//                """;
+
+        Revision rev = revisionRepository.findById(revId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVISION_NOT_FOUND));
+        Revision oldRev = revisionRepository.findById(oldRevId).orElseThrow(() -> new BusinessLogicException(ExceptionCode.REVISION_NOT_FOUND));
+
+        String oldText = oldRev.getContent().getText();
+        String text = rev.getContent().getText();
+
+        LinkedList<Diff> diffs = diffMatchPatch.diff_main(oldText, text);
+//        System.out.println("########################");
+//        System.out.println("두개의 차이 " + diffMatchPatch.diff_levenshtein(diffs));
+//        System.out.println("차이" + diffMatchPatch.diff_length(diffs));
+        diffMatchPatch.diff_cleanupSemantic(diffs);
+//        System.out.println(diffs);
+
+        String html = diffMatchPatch.diff_prettyHtml(diffs);
+
+        return diffs;
+    }
+
+    public String diffHtml(long revId, long oldRevId){
+        String text = """
                 ----
-                {{{#!wiki style="text-align:center"	
+                {{{#!wiki style="text-align:center"
                 '''{{{+2 여러분이 가꾸어 나가는 {{{#00a495,#E69720 지식의 나무}}}}}}'''
                 [[나무위키|{{{#00a495,#E69720 나무위키}}}]]에 오신 것을 환영합니다!
                 [[:파일:nogray.png|{{{#ffffff,#E69720 다크는 회색이 아니라 검정입니다!}}}]]
@@ -114,12 +176,14 @@ public class RevisionService {
                 [include(틀:운영토론)][[분류:나무위키]]
                 ## 운영 토론이나 운영 알림은 해당 틀을 편집해 주시기 바랍니다.
                 """;
-        LinkedList<Diff> diffs = diffMatchPatch.diff_main(oldText, newText);
-        System.out.println("########################");
-        System.out.println("두개의 차이 " + diffMatchPatch.diff_levenshtein(diffs));
-        System.out.println("차이" + diffMatchPatch.diff_length(diffs));
+        LinkedList<Diff> diffs = diffMatchPatch.diff_main(oldText, text);
+//        System.out.println("########################");
+//        System.out.println("두개의 차이 " + diffMatchPatch.diff_levenshtein(diffs));
+//        System.out.println("차이" + diffMatchPatch.diff_length(diffs));
         diffMatchPatch.diff_cleanupSemantic(diffs);
-        System.out.println(diffs);
-        return diffs;
+//        System.out.println(diffs);
+
+        String html = diffMatchPatch.diff_prettyHtml(diffs);
+        return html;
     }
 }
