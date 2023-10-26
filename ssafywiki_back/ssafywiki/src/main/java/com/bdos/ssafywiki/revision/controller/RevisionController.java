a40acf5ca2bd89009388b9cd5f8684799eccb27f
조현덕 <gusejr4547@gmail.com>
2023-10-26 11:00:14 +0900

    ✨ Feat: 버전 목록 반환 API

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/controller/RevisionController.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/revision/controller/RevisionController.java
@@ -2,6 +2,7 @@ package com.bdos.ssafywiki.revision.controller;
 
 import com.bdos.ssafywiki.revision.dto.RevisionDto;
 import com.bdos.ssafywiki.revision.entity.Revision;
+import com.bdos.ssafywiki.revision.mapper.RevisionMapper;
 import com.bdos.ssafywiki.revision.service.RevisionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -14,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Tag(name = "버전 관리 API")
 @RestController
 @RequiredArgsConstructor
@@ -21,17 +24,18 @@ import org.springframework.web.bind.annotation.*;
 public class RevisionController {
 
     private final RevisionService revisionService;
+    private final RevisionMapper revisionMapper;
 
     @Operation(summary = "문서 버전 목록", description = "두개의 버전을 비교합니다.")
     @GetMapping("/{docs_id}")
-    public ResponseEntity getHistory(
+    public ResponseEntity<List<RevisionDto.Version>> getHistory(
             @PathVariable("docs_id") long docsId,
             @PageableDefault(size = 30, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
 
         Page<Revision> revisionPage = revisionService.getHistory(docsId, pageable);
-        System.out.println(revisionPage.getContent());
+        List<Revision> revisionList= revisionPage.getContent();
 
-        return new ResponseEntity(HttpStatus.OK);
+        return new ResponseEntity(revisionMapper.toVersionList(revisionList), HttpStatus.OK);
     }
 
     @Operation(summary = "버전 비교", description = "두개의 버전을 비교합니다.")
