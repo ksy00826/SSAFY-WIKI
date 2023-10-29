d030268296902f6263c05de8f1937ca85673569b
권선근 <kss4037@gmail.com>
2023-10-29 22:49:49 +0900

    Update ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/configuration/RedisConfig.java

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/configuration/RedisConfig.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/configuration/RedisConfig.java
@@ -7,6 +7,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Getter
 @Configuration
