6e902555a3b2c2180799673e622d31084c2584ae
권선근 <kss4037@gmail.com>
2023-10-30 08:56:29 +0900

    Update ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/configuration/RedisConfig.java

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/configuration/RedisConfig.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/configuration/RedisConfig.java
@@ -18,10 +18,10 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("${spring.cache.redis.host}")
+    @Value("${spring.redis.host}")
     private String host;
 
-    @Value("${spring.cache.redis.port}")
+    @Value("${spring.redis.port}")
     private int port;
 
     @Bean
