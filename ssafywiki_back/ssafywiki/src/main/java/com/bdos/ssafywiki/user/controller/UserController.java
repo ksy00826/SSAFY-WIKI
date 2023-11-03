40aa7b033c4b2c9ea28d0860428b2a5f03cfeb19
kss4037 <49977437+kss4037@users.noreply.github.com>
2023-11-03 14:39:23 +0900

    :sparkles: feat: 유저정보 조회 정보 최적화

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/user/controller/UserController.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/user/controller/UserController.java
@@ -1,26 +1,13 @@
 package com.bdos.ssafywiki.user.controller;
 
 import com.bdos.ssafywiki.configuration.jwt.CustomUserDetails;
-import com.bdos.ssafywiki.revision.dto.RevisionDto;
 import com.bdos.ssafywiki.user.dto.UserDto;
-import com.bdos.ssafywiki.user.entity.User;
-import com.bdos.ssafywiki.user.repository.UserRepository;
-import com.bdos.ssafywiki.user.service.AuthenticationService;
 import com.bdos.ssafywiki.user.service.UserService;
-import com.bdos.ssafywiki.util.EmailUtil;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.security.Security;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @Tag(name = "유저 API", description = "마이페이지")
 @RestController
@@ -31,11 +18,11 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/info")
-    public ResponseEntity<User> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<UserDto.Registration> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
         System.out.println(userDetails.getUser());
 
 
-        User response = userService.searchUser(userDetails.getUser().getEmail()).get();
+        UserDto.Registration response = userService.checkUserInfo(userDetails.getUser().getEmail());
         return ResponseEntity.ok(response);
     }
 
