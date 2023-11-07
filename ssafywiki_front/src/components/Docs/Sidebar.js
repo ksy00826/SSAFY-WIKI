66f2bcb492e7c415d828ff5b294086a9519c665c
Pangpyo <kkp0639@gmail.com>
2023-11-07 15:26:54 +0900

    ✨ Feat: 최근 수정 문서 기능

--- ssafywiki_front/src/components/Docs/Sidebar.js
+++ ssafywiki_front/src/components/Docs/Sidebar.js
@@ -1,13 +1,15 @@
 import { Card } from "antd";
 import Discussion from "./Discussion";
-import styles from "./SideBar.module.css"
+import styles from "./SideBar.module.css";
+import RecentDocs from "./RecentDocs";
 const Sidebar = () => {
   return (
     <div>
       <Card>
         <div>최근 수정된 문서</div>
+        <RecentDocs></RecentDocs>
       </Card>
-      <Card bodyStyle={{padding:10}}>
+      <Card bodyStyle={{ padding: 10 }}>
         <div>토론</div>
         <Discussion></Discussion>
       </Card>
