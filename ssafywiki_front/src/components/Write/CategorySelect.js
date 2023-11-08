4d8e80e2ccfefc1f361453861ef67986718cde1f
김승연 <love00826@naver.com>
2023-11-08 10:43:05 +0900

    💄 Design: 문서 분류 선택 반응형 수정

--- ssafywiki_front/src/components/Write/CategorySelect.js
+++ ssafywiki_front/src/components/Write/CategorySelect.js
@@ -31,8 +31,8 @@ const CategorySelect = ({ next, setReadAuth, setWriteAuth, setIsRedirect }) => {
         </Col>
       </Row>
       <Row gutter={24}>
-        <Col span={2}></Col>
-        <Col span={10}>
+        <Col xs={0} sm={1} md={1} lg={2}></Col>
+        <Col xs={6} sm={8} md={8} lg={10}>
           <Card title="SSAFY 공개 문서" size="small">
             <p>SSAFY에 대한 흥미로운 정보!</p>
             <p>외부인 열람이 가능해요</p>
@@ -41,7 +41,7 @@ const CategorySelect = ({ next, setReadAuth, setWriteAuth, setIsRedirect }) => {
             Select
           </Button>
         </Col>
-        <Col span={10}>
+        <Col xs={6} sm={8} md={8} lg={10}>
           <Card title="SSAFY 대외비 문서" size="small">
             <p>SSAFY에 대한 상세한 정보!</p>
             <p>외부인 열람이 불가능해요</p>
@@ -50,11 +50,11 @@ const CategorySelect = ({ next, setReadAuth, setWriteAuth, setIsRedirect }) => {
             Select
           </Button>
         </Col>
-        <Col span={2}></Col>
+        <Col xs={0} sm={1} md={1} lg={2}></Col>
       </Row>
       <Row gutter={24}>
-        <Col span={2}></Col>
-        <Col span={10}>
+        <Col xs={0} sm={1} md={1} lg={2}></Col>
+        <Col xs={6} sm={8} md={8} lg={10}>
           <Card title="일반 문서" size="small">
             <p>여러 목적으로 작성되는 문서!</p>
             <p>인물, CS 등 본인이 원하는 정보를 작성할 수 있어요</p>
@@ -63,7 +63,7 @@ const CategorySelect = ({ next, setReadAuth, setWriteAuth, setIsRedirect }) => {
             Select
           </Button>
         </Col>
-        <Col span={10}>
+        <Col xs={6} sm={8} md={8} lg={10}>
           <Card title="리다이렉트 문서" size="small">
             <p>기존 문서로 연결되는 문서!</p>
             <p>키워드를 등록하여 기존 문서로 바로 이동할 수 있어요</p>
@@ -72,7 +72,7 @@ const CategorySelect = ({ next, setReadAuth, setWriteAuth, setIsRedirect }) => {
             Select
           </Button>
         </Col>
-        <Col span={2}></Col>
+        <Col xs={0} sm={1} md={1} lg={2}></Col>
       </Row>
     </Space>
   );
