3ffcb84c01429d8c4c1c0b63c4905d68fa132158
김승연 <love00826@naver.com>
2023-11-09 23:22:24 +0900

    ✨ Feat: 스트릭 상세 보기 및 타임 라인 생성, 문서 이동

--- ssafywiki_front/src/components/User/LawnGraph.js
+++ ssafywiki_front/src/components/User/LawnGraph.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
-import { Col, Row, Slider, Button, Tooltip } from "antd";
+import {
+  Col,
+  Row,
+  Slider,
+  Button,
+  Tooltip,
+  Divider,
+  Timeline,
+  Card,
+  Avatar,
+} from "antd";
 import { getUserContribute, getUserContributeOneDay } from "utils/UserApi";
+import { FileProtectOutlined, SearchOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+const { Meta } = Card;
 const LawnGraph = () => {
   const [cols, setCols] = React.useState([]);
   const [rows, setRows] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
-
+  const [children, setChildren] = React.useState([]);
+  const [day, setDay] = React.useState([]);
+  const navigate = useNavigate();
   const weekAgo = 6 * 4 - 1;
 
   const colors = ["#EBF3E8", "#D2E3C8", "#B2C8BA", "#86A789"];
@@ -14,17 +29,50 @@ const LawnGraph = () => {
     minWidth: "0",
     minHeight: "0",
     // padding: "10px 10px 10px 10px", // 원하는 패딩 값을 설정
-    margin: "3px 0px",
+    margin: "3px 3px",
     background: "#EBF3E8",
+  };
+
+  const handleDocs = (docsId, title) => {
+    console.log(docsId, title);
+    navigate(`/res/content/${docsId}/${title}`);
   };
 
   const handleClickDay = (e) => {
     console.log(e.target.value);
     const date = e.target.value;
+    setDay(date.substr(0, 10));
     getUserContributeOneDay(date).then((result) => {
       console.log(result);
+
+      const chi = [];
+      result.forEach((doc) => {
+        chi.push({
+          dot: <FileProtectOutlined style={{ color: "green" }} />,
+          children: (
+            <>
+              <b>Create Document </b>
+              <span style={{ fontSize: "smaller", color: "gray" }}>
+                - {doc.createdAt.substr(12)}
+              </span>
+              <p>
+                <Button
+                  type="text"
+                  onClick={() => handleDocs(doc.docsId, doc.title)}
+                >
+                  {doc.title}
+                </Button>
+              </p>
+            </>
+          ),
+        });
+      });
+      setChildren(chi);
     });
   };
+  //   React.useEffect(() => {
+  //     console.log("ch", children);
+  //   }, [children]);
 
   React.useEffect(() => {
     const now = new Date(); //현재 날짜 및 시간
@@ -79,10 +127,7 @@ const LawnGraph = () => {
                 <Tooltip title={cnt + " contribute"} color="black">
                   <Button
                     style={{
-                      minWidth: "0",
-                      minHeight: "0",
-                      // padding: "10px 10px 10px 10px", // 원하는 패딩 값을 설정
-                      margin: "3px 3px",
+                      ...buttonStyle,
                       background: color,
                     }}
                     value={date + "T00:00:00"}
@@ -104,55 +149,48 @@ const LawnGraph = () => {
 
   return (
     <>
-      <Row>
-        {/* {loading ? (
-          <></>
-        ) : (
-          <>
-            {rows.map((index) => {
-              <Col>{rows[index]}</Col>;
-            })}
-          </>
-        )} */}
-        <Col>{rows[0]}</Col>
-        <Col>{rows[1]}</Col>
-        <Col>{rows[2]}</Col>
-        <Col>{rows[3]}</Col>
-        <Col>{rows[4]}</Col>
-        <Col>{rows[5]}</Col>
-        <Col>{rows[6]}</Col>
-        <Col>{rows[7]}</Col>
-        <Col>{rows[8]}</Col>
-        <Col>{rows[9]}</Col>
-        <Col>{rows[10]}</Col>
-        <Col>{rows[11]}</Col>
-        <Col>{rows[12]}</Col>
-        <Col>{rows[13]}</Col>
-        <Col>{rows[14]}</Col>
-        <Col>{rows[15]}</Col>
-        <Col>{rows[16]}</Col>
-        <Col>{rows[17]}</Col>
-        <Col>{rows[18]}</Col>
-        <Col>{rows[19]}</Col>
-        <Col>{rows[20]}</Col>
-        <Col>{rows[21]}</Col>
-        <Col>{rows[22]}</Col>
-      </Row>
+      <Card style={{ marginTop: 16 }} loading={loading}>
+        <Row>
+          <Col>{rows[0]}</Col>
+          <Col>{rows[1]}</Col>
+          <Col>{rows[2]}</Col>
+          <Col>{rows[3]}</Col>
+          <Col>{rows[4]}</Col>
+          <Col>{rows[5]}</Col>
+          <Col>{rows[6]}</Col>
+          <Col>{rows[7]}</Col>
+          <Col>{rows[8]}</Col>
+          <Col>{rows[9]}</Col>
+          <Col>{rows[10]}</Col>
+          <Col>{rows[11]}</Col>
+          <Col>{rows[12]}</Col>
+          <Col>{rows[13]}</Col>
+          <Col>{rows[14]}</Col>
+          <Col>{rows[15]}</Col>
+          <Col>{rows[16]}</Col>
+          <Col>{rows[17]}</Col>
+          <Col>{rows[18]}</Col>
+          <Col>{rows[19]}</Col>
+          <Col>{rows[20]}</Col>
+          <Col>{rows[21]}</Col>
+          <Col>{rows[22]}</Col>
+        </Row>
+      </Card>
+      <Divider orientation="left" orientationMargin="0">
+        <b>Timeline</b>
+        <i> {day != "" ? " - " + day : ""}</i>
+        <br></br>
+      </Divider>
+
+      {children.length != 0 ? (
+        <>
+          <Timeline items={children} style={{ textAlign: "left" }} />
+        </>
+      ) : (
+        <>No Contribute</>
+      )}
     </>
   );
 };
 export default LawnGraph;
-
-{
-  /* {rows ? (
-            rows.map((items, index) => {
-              items.map((item, idx) => {
-                {
-                  item;
-                }
-              });
-            })
-          ) : (
-            <></>
-          )} */
-}
+//
