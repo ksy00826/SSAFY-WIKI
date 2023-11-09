2a8581691b08db30d8b3c07aec258fc9bb39fcab
kss4037 <49977437+kss4037@users.noreply.github.com>
2023-11-09 11:17:50 +0900

    :sparkles: 요청주소 https로 변경

--- ssafywiki_front/src/utils/AxiosConfig.js
+++ ssafywiki_front/src/utils/AxiosConfig.js
@@ -36,7 +36,7 @@ export const axiosSsafygitInstance = axios.create({
 });
 export const axiosElasticInstance = axios.create({
   maxBodyLength: Infinity,
-  baseURL: "http://k9e202a.p.ssafy.io:9200/test_v1.0.0/",
+  baseURL: "https://k9e202a.p.ssafy.io/test_v1.0.0/",
   headers: {
     Accept: "application/json, text/plain, */*",
     "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6",
