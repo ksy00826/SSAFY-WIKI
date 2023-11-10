a1ee4f011252bec5f490487146d8b1099e0af85e
Pangpyo <kkp0639@gmail.com>
2023-11-10 15:53:05 +0900

    ✨ Feat: mdx 문법 에러처리

--- ssafywiki_front/src/components/Common/MarkDownRenderer.js
+++ ssafywiki_front/src/components/Common/MarkDownRenderer.js
@@ -4,23 +4,30 @@ import style from "./Component.module.css";
 import { getSearchDoc } from "utils/DocsApi";
 import { useState, useEffect } from "react";
 const MoveDocs = ({ children, docs }) => {
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
 
   const onSearch = (keyword) => {
-    getSearchDoc(keyword).then((response) => {
-      const output = response.data.hits.hits;
-      const newSearched = output.map((element) => {
-        return { label: element._source.docs_title, value: element._source.docs_id };
-      });
-      if (newSearched.length > 0 && newSearched[0].label === keyword) {
-        setUrl(`/res/content/${newSearched[0].value}/${newSearched[0].label}`);
-      } else {
+    getSearchDoc(keyword)
+      .then((response) => {
+        const output = response.data.hits.hits;
+        const newSearched = output.map((element) => {
+          return {
+            label: element._source.docs_title,
+            value: element._source.docs_id,
+          };
+        });
+        if (newSearched.length > 0 && newSearched[0].label === keyword) {
+          setUrl(
+            `/res/content/${newSearched[0].value}/${newSearched[0].label}`
+          );
+        } else {
+          setUrl(`/res/list?title=${keyword}`);
+        }
+      })
+      .catch((error) => {
+        console.error("Error searching documents:", error);
         setUrl(`/res/list?title=${keyword}`);
-      }
-    }).catch((error) => {
-      console.error('Error searching documents:', error);
-      setUrl(`/res/list?title=${keyword}`);
-    });
+      });
   };
 
   // onSearch 함수를 호출해서 url 상태를 설정
@@ -42,18 +49,38 @@ const Note = ({ children }) => {
   const [isTooltipVisible, setTooltipVisible] = useState(false);
 
   return (
-    <span className={style.noteContainer}
-          onMouseEnter={() => setTooltipVisible(true)} // 마우스를 올리면 툴팁을 보여줍니다.
-          onMouseLeave={() => setTooltipVisible(false)} // 마우스를 떼면 툴팁을 숨깁니다.
+    <span
+      className={style.noteContainer}
+      onMouseEnter={() => setTooltipVisible(true)} // 마우스를 올리면 툴팁을 보여줍니다.
+      onMouseLeave={() => setTooltipVisible(false)} // 마우스를 떼면 툴팁을 숨깁니다.
     >
       <span className={style.Note}>[주]</span>
-      {isTooltipVisible && <span className={style.tooltip}>{children}</span>}
+      {isTooltipVisible && (
+        <div className={style.tooltip}>
+          <span className={style.toolText}>{children}</span>
+        </div>
+      )}
     </span>
   );
 };
 
+const LinkTo = ({ children, link }) => {
+  const absoluteLink =
+    link.includes("http://") || link.includes("https://")
+      ? link
+      : `https://${link}`;
+  console.log(absoluteLink);
+  return (
+    <a href={absoluteLink} className={style.LinkTo}>
+      #{children}
+    </a>
+  );
+};
+
 const components = {
-  MoveDocs, Note 
+  MoveDocs,
+  Note,
+  LinkTo,
 };
 
 const MarkdownRenderer = ({ content }) => {
