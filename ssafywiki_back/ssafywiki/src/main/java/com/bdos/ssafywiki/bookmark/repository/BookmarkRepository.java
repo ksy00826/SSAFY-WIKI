201d19f2c264048e62df044a21a6b8358d6c2801
young eun <duddms0190@naver.com>
2023-11-12 00:28:35 +0900

    ✨ Feat: 북마크 기능 추가 + security 권한별 기능 설정

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/bookmark/repository/BookmarkRepository.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/bookmark/repository/BookmarkRepository.java
@@ -1,6 +1,8 @@
 package com.bdos.ssafywiki.bookmark.repository;
 
 import com.bdos.ssafywiki.bookmark.entity.Bookmark;
+import com.bdos.ssafywiki.document.entity.Document;
+import com.bdos.ssafywiki.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,9 +14,12 @@ import java.util.Optional;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
-    @Query("select b from Bookmark b join fetch b.document join fetch b.user where b.user.id = :userId")
-    Page<Bookmark> findAllByUserId(Long userId, Pageable pageable);
+    Page<Bookmark> findAllByUser(User user, Pageable pageable);
 
     @Query("select b from Bookmark b join fetch b.document where b.document.id = :docsId")
     Optional<Bookmark> findByDocsId(Long docsId);
+
+    Optional<Bookmark> findByDocumentAndUser(Document document, User user);
+
+    int countByDocument(Document document);
 }
