19c522baafcf82cab5fc5e18c4a3a474de6fd6e2
김승연 <love00826@naver.com>
2023-11-13 16:43:57 +0900

    🐛 Fix: 내 문서 생성 버그 수정

--- ssafywiki_front/src/components/User/MyPage.js
+++ ssafywiki_front/src/components/User/MyPage.js
@@ -15,12 +15,7 @@ import {
 import { getUserProfile } from "utils/UserApi";
 import { AuditOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
-import {
-  getSearchDoc,
-  getDocsContent,
-  getRedirectKeyword,
-  createDocsBeforeLogin,
-} from "utils/DocsApi";
+import { getSearchDoc, createDocs } from "utils/DocsApi";
 
 import UserNavbar from "components/Common/UserNavbar";
 import LawnGraph from "./LawnGraph";
@@ -51,7 +46,7 @@ const MyPage = () => {
   }, []);
 
   const makeDefaultDocs = () => {
-    createDocsBeforeLogin({
+    createDocs({
       title: info.name + (` (${info.number})` || ""),
       content: `### Hi there 👋\nI'm ${githubId}, a software engineer 💻 currently working at [Takeaway.com](https://www.ssafy.com/) 🍲🥡\n\nI have a passion for clean code, Java, teaching, PHP, Lifeguarding and Javascript\n\n# Here are some good things to introduce yourself\n###  change several \"${githubId}\" to your github Id\n# 문서를 꾸미기 위한 마크다운 뱃지들\n![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white)\n![Spring](https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white)\n![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)\n![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)\n![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)\n\nhttps://ileriayo.github.io/markdown-badges/#markdown-badges\n\n# 깃허브에서 사용한 언어 그래프\n[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=${githubId}&layout=compact)](https://github.com/anuraghazra/github-readme-stats)\n\n# 깃허브 스탯\n[![${githubId}'s github stats](https://github-readme-stats.vercel.app/api?username=${githubId}&show_icons=true&theme=default)](https://github.com/${githubId}/)\n\n### thema can be one of [ dark radical merko gruvbox tokyonight ondark cobalt synthwave highcontrast dracula ]\n\n# 하이퍼링크\n[Email 📬](mailto:hallo@dannyverpoort.nl)\n[LinkedIn 💼](https://linkedin.com/in/dannyverpoort)\n[Twitter 🐦](https://twitter.com/dannyverp)\n[Website 🌍](https://dannyverpoort.dev/)`,
       categories: [info.roll],
@@ -72,7 +67,7 @@ const MyPage = () => {
   const handleUserDocs = () => {
     const keyword = userDocs;
 
-    console.log("onSearch");
+    console.log("onSearch", userDocs);
     getSearchDoc(keyword).then((data) => {
       var output = data.data.hits.hits;
       console.log("output", output);
@@ -82,12 +77,15 @@ const MyPage = () => {
         return {
           label: element._source.docs_title,
           value: element._source.docs_id,
+          isDeleted: element._source.docs_is_deleted,
         };
       });
+      console.log(newSearched[0].label === keyword);
+      console.log(newSearched[0].isDeleted == false);
       if (
         newSearched.length > 0 &&
         newSearched[0].label === keyword &&
-        newSearched[0].docs_is_deleted === false
+        newSearched[0].isDeleted == false
       ) {
         navigate(
           `/res/content/${newSearched[0].value}/${newSearched[0].label}`
