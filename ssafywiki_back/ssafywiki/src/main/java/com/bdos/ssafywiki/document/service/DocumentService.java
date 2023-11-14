1da9256120adaf43b8346fae9114d0526d562d6e
Pangpyo <kkp0639@gmail.com>
2023-11-14 16:53:02 +0900

    🎨 Style: 사용하지 않는 import 제거

--- ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/document/service/DocumentService.java
+++ ssafywiki_back/ssafywiki/src/main/java/com/bdos/ssafywiki/document/service/DocumentService.java
@@ -40,7 +40,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
